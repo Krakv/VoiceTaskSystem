c6fced61f98392543626ff9e20660e965c426a3d
Igor Tokmakov <104667117+Krakv@users.noreply.github.com>
2026-04-04 14:35:37 +0500

    feat: added an ability to create tasks from tasks page

--- src/frontend/voice-task/src/components/TaskManager.tsx
+++ src/frontend/voice-task/src/components/TaskManager.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { taskApi } from "@/api/task.api";
 import {Card} from "@/components/ui/card.tsx";
 import {Button} from "@/components/ui/button.tsx";
+import {useNavigate} from "react-router-dom";
+import {Plus} from "lucide-react";
 
 interface Task {
     taskId: string;
@@ -14,6 +16,7 @@ interface Task {
 export const TaskManager = () => {
     const [tasks, setTasks] = useState<Task[]>([]);
     const [error, setError] = useState("");
+    const navigate = useNavigate();
 
     const fetchTasks = async () => {
         try {
@@ -27,6 +30,11 @@ export const TaskManager = () => {
     useEffect(() => {
         fetchTasks();
     }, []);
+
+    const handleCreate = () => {
+        navigate("/create");
+        console.log("Создать новую задачу");
+    };
 
     const handleDelete = async (taskId: string) => {
         try {
@@ -61,6 +69,13 @@ export const TaskManager = () => {
                     </Card>
                 ))
             )}
+
+            <Button
+                onClick={handleCreate}
+                className="sticky bottom-4 right-4 rounded-full w-14 h-14 p-0 flex items-center justify-center shadow-lg bg-blue-600 text-white hover:bg-blue-700"
+            >
+                <Plus className="w-6 h-6" />
+            </Button>
         </div>
     );
 };
