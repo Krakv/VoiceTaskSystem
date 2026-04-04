76108259f2b9d4b234a096c35fd89dd64f394e7d
Igor Tokmakov <104667117+Krakv@users.noreply.github.com>
2026-04-04 14:33:25 +0500

    feat: changed routes

--- src/frontend/voice-task/src/router/index.ts
+++ src/frontend/voice-task/src/router/index.ts
@@ -1,16 +1,15 @@
 import Login from '@/pages/Login'
-import {DefaultPage} from "@/pages/DefaultPage.tsx";
 import Register from "@/pages/Register.tsx";
 import {MainPage} from "@/pages/MainPage.tsx";
 import {TaskCreateForm} from "@/components/TaskCreateForm.tsx";
 
 export const privateRoutes = [
-    { path: '/main', component: MainPage },
+    { path: '/tasks', component: MainPage },
     { path: '/create', component: TaskCreateForm },
 ];
 
 export const publicRoutes = [
-    { path: '*', component: DefaultPage },
+    { path: '*', component: Login },
     { path: '/login', component: Login },
     { path: '/register', component: Register },
 ];
